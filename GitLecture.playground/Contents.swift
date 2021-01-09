f164414a0b45b02ab7cb4534cd3f2e3a0d1f059d
tittaporn <tittaporn_saelee@icloud.com>
2021-01-08 18:04:27 -0600

    another playground

---
+++ GitLecture.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Day 5 : Afternoon Lecture Git by Srom Martinez
+/*
+ 
+ What is Git? >> Git =! GitHub
+    Git is..
+    - A distributed version-control system for tracking changes in source code during sofeware develpment.
+    - It is designed for coofinating work among programmers, but it  can be used to track changs in any set of files.
+    - Just be careful while using it.
+ 
+ Terminal is command line tool.
+    >> command + space == for finder.
+    >> command + K == for cleaning terminal screen
+    
+ pwd == show the path. "where you at" ex./Users/tittapornsaelee
+ cd == change dicrectory
+ ls == list
+ cd .. == go back to last dicractory, jumped up one at a time
+ 
+ >> git repository >> this is what we gonna use a lot
+ 
+ For saving in the could and for working in a team. cloud safty and ability to work with people.
+ 
+ It is a free version for using on Github.
+ 
+ Branches >> We can work in seperate and many branches and combine it at the end.
+ 
+ When we cloning the git, you have an origin branch from github
+ ,but you will have local copy in your computer.
+ 
+ Keywords >> Git that we will using offen
+
+ git branch
+ git clone
+ git status
+ git add
+ git log
+ git commit -m  >> ex."Added a descrption to README." Everytime commit, you need to comment what you do.
+ git push origin master >> pushing it to the gitHub git repository
+ 
+ pull >> pull the data after the local push
+
+ commit >> for keep save changs in your project.
+ 
+ 
+ 
+ */
